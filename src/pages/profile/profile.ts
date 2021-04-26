e54fa61a48a52a994ec9392a5f0daec6da17ba73
Eduardo <duh.henrike@gmail.com>
2021-04-26 15:53:08 -0300

    Tratando erro 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -30,8 +30,14 @@ export class ProfilePage {
             this.cliente = response;
             this.getProfileImageIfExists();
           }, 
-          error => {}
+          error => {
+            if (error.status == 403) {
+              this.navCtrl.setRoot('HomePage');
+            }
+          }
         );
+    } else {
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
