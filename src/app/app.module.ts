6f61a0c758b0e84b897ba2290d970ede1c0fe643
Eduardo <duh.henrike@gmail.com>
2021-04-28 18:53:04 -0300

    Busca de produtos por categoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
 import { ClienteService } from '../services/cliente.service';
 import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
+import { ProdutoService } from '../services/domain/produto.service';
 
 @NgModule({
   declarations: [
@@ -37,6 +38,7 @@ import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
     StorageService,
     CategoriaService,
     ClienteService,
+    ProdutoService,
   ]
 })
 export class AppModule {}
